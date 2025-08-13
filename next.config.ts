efaf0c1fc71042299179c12bf84c3ad09b7be726
mohammeddl <mohammed_daali2@um5.ac.ma>
2025-08-13 21:41:14 +0100

    Add initial project structure with configuration files, new components, and assets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+const withNextIntl = require('next-intl/plugin')();
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = withNextIntl({
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en'
+  }
+});
